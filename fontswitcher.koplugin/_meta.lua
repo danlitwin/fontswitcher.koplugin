3edd3c050887d5c580578bb9c19926dbbb4a6832
Dan Litwin <danlitwin@users.noreply.github.com>
2026-06-05 15:32:36 -0400

    Change author name in _meta.lua

--- fontswitcher.koplugin/_meta.lua
+++ fontswitcher.koplugin/_meta.lua
@@ -3,5 +3,5 @@ return {
     fullname = "Font Switcher",
     description = [[Switch fonts via gestures or Top Menu shortcut in reflowable documents.]],
     version = "1.0",
-    author = "right9code",
+    author = "danlitwin",
 }
